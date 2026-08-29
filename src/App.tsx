8bbd7cfcaf5ee75c4a415d323b836e8e30852c0b
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-08-29 10:06:01 -0700

    R16: mount interface hierarchy as final authority

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import './runtimeRailR4.css';
 import './omegaSovereignVisualR10.css';
 import './navigationButtonPolishR11.css';
 import './omegaProfessionalR13.css';
+import './omegaInterfaceR16.css';
 import {RUNTIME_IDENTITY} from './runtimeIdentity';
 import OmegaHome from './OmegaHome';
 import OmegaLauncher from './OmegaLauncher';
