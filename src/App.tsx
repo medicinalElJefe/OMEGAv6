e64344619145a9243c563f2ef0a32b9919f8e469
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-08-29 06:55:25 -0700

    style: activate collapsed runtime rail layer

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './omegaVisualIdentity.css';
 import './uxSurpassR2.css';
 import './uxSurpassR3.css';
 import './experienceR4.css';
+import './runtimeRailR4.css';
 import {RUNTIME_IDENTITY} from './runtimeIdentity';
 import OmegaHome from './OmegaHome';
 import OmegaLauncher from './OmegaLauncher';
