32c136a9e965071b9b4861ea14c413fbff9034cb
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-07 09:13:42 -0700

    R183 bind canonical API transport before application boot

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import {installCanonicalApiTransportR183} from './canonicalApiTransportR183';
 import './index.css';
 import './b036.css';
 import './b037.css';
+
+installCanonicalApiTransportR183();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
