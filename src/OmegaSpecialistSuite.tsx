109da115c08d08b241558c6b35ea185ceecf9346
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-07 09:50:41 -0700

    R184 mount operational correlation mesh above retained Convergence tools

--- src/OmegaSpecialistSuite.tsx
+++ src/OmegaSpecialistSuite.tsx
@@ -11,6 +11,7 @@ import OmegaCapabilityFieldR138 from './OmegaCapabilityFieldR138';
 import ReflexAutonomicR164 from './ReflexAutonomicR164';
 import AppliedCalculusR168 from './AppliedCalculusR168';
 import FullRestorationConvergenceR168 from './FullRestorationConvergenceR168';
+import OmegaCorrelationMeshR184 from './OmegaCorrelationMeshR184';
 
 type Props={panel:string;record:any;state:any;address:number;onAddress:(n:number)=>void;onNavigate:(p:string)=>void;status:any;restore:any;uiMode:any;onUiMode:(m:any)=>void};
 
@@ -19,6 +20,7 @@ export default function OmegaSpecialistSuite(props:Props){
  const capability=<OmegaCapabilityFieldR138 panel={panel} record={record} address={address} onAddress={onAddress} onNavigate={onNavigate} status={status} restore={restore}/>;
  const wrap=(content:any)=><div className='r138-capability-first'>{capability}{content}</div>;
  if(panel==='Convergence')return wrap(<div>
+  <OmegaCorrelationMeshR184 record={record} address={address} onNavigate={onNavigate}/>
   <AppliedCalculusR168/>
   <FullRestorationConvergenceR168 record={record} address={address} onNavigate={onNavigate}/>
   <OmegaMaximumCockpitR126 record={record} state={state} address={address} onAddress={onAddress} onNavigate={onNavigate}/>
