aceb0566ab65b44e1209c7b6464687ea875a4490
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-08 18:00:57 -0700

    Expose native GUNW evidence processor in heavy SAR API

--- sar-accumulation-studio/processor/omega_sar/__init__.py
+++ sar-accumulation-studio/processor/omega_sar/__init__.py
@@ -6,11 +6,13 @@ browse imagery, proxy Earth charts, test fixtures, or Atlas inference into SAR m
 
 from .nisar import NisarProduct, ProductInspection, ScienceDataset
 from .stack import accumulate_geocoded_stack, render_stack_gif
+from .gunw import GUNWResult, read_gunw, export_gunw, phase_to_range_change_m
 from .proof import sha256_file, deterministic_digest, write_json
 
 __all__ = [
     'NisarProduct','ProductInspection','ScienceDataset',
     'accumulate_geocoded_stack','render_stack_gif',
+    'GUNWResult','read_gunw','export_gunw','phase_to_range_change_m',
     'sha256_file','deterministic_digest','write_json',
 ]
-__version__ = '0.3.0'
+__version__ = '0.4.0'
