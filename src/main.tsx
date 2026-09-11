2f55ca3801f7a1916ed8367a806cab227d25c644
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-10 18:10:08 -0700

    R277 install live Earth Woven enhancer

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import {installCanonicalApiTransportR183} from './canonicalApiTransportR183';
+import {installEarthVisualEnhancerR277} from './earthVisualEnhancerR277';
 import './index.css';
 import './b036.css';
 import './b037.css';
 
 installCanonicalApiTransportR183();
+installEarthVisualEnhancerR277();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
