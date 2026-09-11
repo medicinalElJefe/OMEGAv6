660ebf2e652e8b09d368c92c94d0a34954782b2a
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-10 21:14:30 -0700

    R280: export integrated SAR truth capability surface

---
+++ src/sarTruthIndexR280.ts
@@ -0,0 +1,7 @@
+export * from'./sarTruthR280';
+export * from'./sarMathR280';
+export * from'./sarPolInSARR280';
+export * from'./sarAccumulationR280';
+export * from'./sarDataCubeR280';
+export * from'./sarFusionR280';
+export * from'./sarSourcePlannerR280';
