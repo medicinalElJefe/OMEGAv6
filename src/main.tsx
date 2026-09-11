d552427182dc9ce96ae2f2b6ad5d8985a2f40546
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-10 18:10:17 -0700

    R277 keep live visual enhancer route-deferred

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import {installCanonicalApiTransportR183} from './canonicalApiTransportR183';
-import {installEarthVisualEnhancerR277} from './earthVisualEnhancerR277';
 import './index.css';
 import './b036.css';
 import './b037.css';
 
 installCanonicalApiTransportR183();
-installEarthVisualEnhancerR277();
+void import('./earthVisualEnhancerR277').then(m=>m.installEarthVisualEnhancerR277()).catch(()=>{});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
