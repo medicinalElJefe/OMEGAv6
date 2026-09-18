f4bbcb8ba8c3c3bdf5e9af31ca697d2e8755a6a6
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-17 19:00:30 -0700

    R319.7 pin child probes to candidate by Worker dictionary

--- scripts/cloudflare-version-override-fetch.mjs
+++ scripts/cloudflare-version-override-fetch.mjs
@@ -11,7 +11,7 @@ if(candidateVersion&&canonicalBase){
     try{targetOrigin=new URL(rawUrl,canonicalBase).origin}catch{}
     if(targetOrigin!==canonicalOrigin)return nativeFetch(input,init);
     const headers=new Headers(init?.headers||(typeof input==='object'&&input?.headers?input.headers:undefined));
-    headers.set('cf-workers-version-overrides',candidateVersion);
+    headers.set('cf-workers-version-overrides',`${workerName}="${candidateVersion}"`);
     return nativeFetch(input,{...init,headers});
   };
 }
