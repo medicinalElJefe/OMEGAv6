ec5a474e65ef43dad22a3c23345153377d67a38f
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-19 13:04:33 -0700

    R329 bind B05 promotion receipt to convergence master

--- src/convergenceMasterR314.ts
+++ src/convergenceMasterR314.ts
@@ -1,6 +1,7 @@
 import {OMEGA_CAPABILITY_AUTHORITY,capabilityReality,type CapabilityReality} from './capabilityAuthority';
 import {ARCHIVE_GENOME_ALL_ROWS_R288,archiveGenomeAllSummaryR288} from './archiveGenomeLedgerR288b';
 import {R328_SOURCE_CANON_RECEIPT} from './system/sourceExactCanonR328';
+import {R329_B05_PROMOTION_RECEIPT} from './system/authoritativeStateSpineR329';
 
 export const R314_CONVERGENCE_SCHEMA='OMEGA_CONVERGENCE_MASTER_R314' as const;
 export const R314_CONVERGENCE_REVISION='R314' as const;
@@ -125,6 +126,7 @@ export function buildConvergenceMasterR314(){
   ],
   archiveSummary:archiveGenomeAllSummaryR288(),
   sourceExactCanon:{revision:'R328',...R328_SOURCE_CANON_RECEIPT,semanticSource:true,implementationCanonSeparate:true,canonicalAdmission:false},
+  promotionReceipts:[R329_B05_PROMOTION_RECEIPT],
   residual:convergenceResidualR314(),
   charts:{current,incomplete,build},
  };
