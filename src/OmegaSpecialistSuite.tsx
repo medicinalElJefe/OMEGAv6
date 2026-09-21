11e645f10cc7545195c932b6bbdbd87e32a5f91a
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-21 07:44:05 -0700

    R349: surface hardware Woven field in canonical Convergence route

--- src/OmegaSpecialistSuite.tsx
+++ src/OmegaSpecialistSuite.tsx
@@ -18,6 +18,7 @@ import FullRestorationConvergenceR168 from './FullRestorationConvergenceR168';
 import RecursiveSelfBuildR240 from './RecursiveSelfBuildR240';
 import CalculusAddressFabricR240 from './CalculusAddressFabricR240';
 import OmegaUnifiedConvergenceR348 from './OmegaUnifiedConvergenceR348';
+import OmegaHardwareFieldR349 from './OmegaHardwareFieldR349';
 
 type Props={panel:string;record:any;state:any;address:number;onAddress:(n:number)=>void;onNavigate:(p:string)=>void;status:any;restore:any;uiMode:any;onUiMode:(m:any)=>void};
 
@@ -31,6 +32,7 @@ export default function OmegaSpecialistSuite(props:Props){
   <OmegaResearchAdvancementR316/>
   <RecursiveSelfBuildR240/>
   <OmegaUnifiedConvergenceR348 record={record} status={status}/>
+  <OmegaHardwareFieldR349 address={address}/>
   <FullRestorationConvergenceR168 record={record} address={address} onNavigate={onNavigate}/>
   <OmegaMaximumCockpitR126 record={record} state={state} address={address} onAddress={onAddress} onNavigate={onNavigate}/>
   <details className='r121-legacy-convergence' open><summary>R164 returned reflex → autonomic swarm · replay / residual carry / governed recruitment</summary><ReflexAutonomicR164/></details>
